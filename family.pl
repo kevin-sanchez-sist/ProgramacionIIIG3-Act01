32ee6ca913aafcdbaf04a8632f9c577feeb99edf
cxrdxnx <mateoosoriocardonadev@gmail.com>
2025-08-26 10:34:45 -0500

    mejorando los funtores para más coherencia

--- family.pl
+++ family.pl
@@ -15,43 +15,71 @@ mama_de(jacqueline,selma).
 mama_de(marge,bart).
 mama_de(marge,lisa).
 mama_de(marge,maggie).
-mama_de(patty,ling).
+mama_de(selma,ling).
 
-hombre_es(abraham).
-hombre_es(clancy).
-hombre_es(homero).
-hombre_es(bart).
-hombre_es(herbert).
-
-
+es_hombre(abraham).
+es_hombre(clancy).
+es_hombre(homero).
+es_hombre(bart).
+es_hombre(herbert).
 
 
-mujer_es(mona).
-mujer_es(jacqueline).
-mujer_es(patty).
-mujer_es(marge).
-mujer_es(selma).
-mujer_es(lisa).
-mujer_es(maggie).
-mujer_es(selma).
-mujer_es(ling).
+es_mujer(mona).
+es_mujer(jacqueline).
+es_mujer(patty).
+es_mujer(marge).
+es_mujer(selma).
+es_mujer(lisa).
+es_mujer(maggie).
+es_mujer(selma).
+es_mujer(ling).
 
 progenitor_de(A,B):-papa_de(A,B).
 progenitor_de(A,B):-mama_de(A,B).
 
-abuelo_de(A,B) :- papa_de(C,B) , papa_de(A,C).
-abuela_de(A,B):- mama_de(C,B) , mama_de(A,C).
+abuelo_de(A,B) :- 
+    papa_de(C,B) , 
+    progenitor_de(A,C).
 
-hermano_es(A,B) :-  A \= B, hombre_es(A),progenitor_de(C,A),progenitor_de(C,B).
-hermana_es(A,B) :-  A \= B, mujer_es(A),progenitor_de(C,A),progenitor_de(C,B).
+abuela_de(A,B):- 
+    mama_de(C,B),
+    progenitor_de(A,C).
+
+es_hermano(A,B) :-  
+    A \= B,
+    es_hombre(A),
+    progenitor_de(C,A),
+    progenitor_de(C,B).
+
+es_hermana(A,B) :-  
+    A \= B, 
+    es_mujer(A),
+    progenitor_de(C,A),
+    progenitor_de(C,B).
 
 
-tio_es(A,B):- A \= B, hombre_es(A),hermano_es(A,C),progenitor_de(C,B).
-tia_es(A,B):- A \= B, mujer_es(A),progenitor_de(C,B),hermana_es(A,C).
+es_tio(A,B):-  
+    es_hombre(A),
+    hermano_es(A,C),
+    progenitor_de(C,B).
+
+es_tia(A,B):- 
+    es_mujer(A),
+    progenitor_de(C,B),
+    hermana_es(A,C).
 
 
-primo_es(A,B) :-    hombre_es(A), progenitor_de(C,B),tio_es(C,A); progenitor_de(C,B),tia_es(C,A).
-prima_es(A,B) :-    hombre_es(A), progenitor_de(C,B),tio_es(C,A); progenitor_de(C,B),tia_es(C,A).
+es_primo(A,B) :-    
+    es_hombre(A), 
+    progenitor_de(C,B),
+    es_tio(C,A); 
+    es_tia(C,A).
+    
+es_prima(A,B) :-    
+    es_mujer(A), 
+    progenitor_de(C,B),
+    es_tio(C,A); 
+    es_tia(C,A).
 
 
 
